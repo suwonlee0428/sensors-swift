5a4e5ec245e019c0b582fe1be31cf7ea67b75437
Stefano <stdemicheli@protonmail.com>
2022-11-26 04:11:07 +0100

    Fixes compile errors.

--- Sources/SwiftySensors/Sensor.swift
+++ Sources/SwiftySensors/Sensor.swift
@@ -275,7 +275,7 @@ extension Sensor: CBPeripheralDelegate {
     
     /// :nodoc:
     public func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        guard let service = services[characteristic.service.uuid.uuidString] else { return }
+        guard let cbService = characteristic.service, let service = services[cbService.uuid.uuidString] else { return }
         guard let char = service.characteristics[characteristic.uuid.uuidString] else { return }
         if char.cbCharacteristic !== characteristic {
             char.cbCharacteristic = characteristic
@@ -286,7 +286,7 @@ extension Sensor: CBPeripheralDelegate {
     
     /// :nodoc:
     public func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
-        guard let service = services[characteristic.service.uuid.uuidString] else { return }
+        guard let cbService = characteristic.service, let service = services[cbService.uuid.uuidString] else { return }
         guard let char = service.characteristics[characteristic.uuid.uuidString] else { return }
         if char.cbCharacteristic !== characteristic {
             char.cbCharacteristic = characteristic
