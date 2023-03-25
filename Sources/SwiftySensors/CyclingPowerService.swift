cfe07194d8c047dee0c5d4e98cfa7e845bf5a519
suwonlee0428 <88119518+suwonlee0428@users.noreply.github.com>
2023-03-25 13:42:39 -0700

    Update CyclingPowerService.swift

--- Sources/SwiftySensors/CyclingPowerService.swift
+++ Sources/SwiftySensors/CyclingPowerService.swift
@@ -23,8 +23,7 @@ open class CyclingPowerService: Service, ServiceProtocol {
         Feature.uuid:           Feature.self,
         Vector.uuid:            Vector.self,
         SensorLocation.uuid:    SensorLocation.self,
-        ControlPoint.uuid:      ControlPoint.self,
-        WahooTrainer.uuid:      WahooTrainer.self
+        ControlPoint.uuid:      ControlPoint.self
     ]
     
     public var measurement: Measurement? { return characteristic() }
